c8be18cfd65c1dffeacec3d5e946baf7831dae05
Jardel Rodrigues <rodriguinhosilvards96@gmail.com>
2023-12-23 21:11:25 -0300

    OrderItem, OrderItemPk

--- src/main/java/com/softstrem/dscommerce/entities/Product.java
+++ src/main/java/com/softstrem/dscommerce/entities/Product.java
@@ -2,6 +2,7 @@ package com.softstrem.dscommerce.entities;
 
 import java.io.Serializable;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Column;
@@ -12,31 +13,32 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(name = "name")
 	private String name;
-	
+
 	@Column(columnDefinition = "TEXT")
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	
+
 	@ManyToMany
-	@JoinTable(name = "tb_product_category",
-			joinColumns = @JoinColumn(name = "product_id"),
-			inverseJoinColumns = @JoinColumn(name = "category_id"))
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Product() {
 
@@ -91,8 +93,20 @@ public class Product implements Serializable{
 		this.imgUrl = imgUrl;
 	}
 
-	public Set<Category> getCotegories() {
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public Set<Category> getCategories() {
 		return categories;
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+
+	public List<Order> getOrder() {
+		return items.stream().map(x -> x.getOrder()).toList();
 	}
 
 }
