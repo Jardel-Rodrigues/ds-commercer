a783d8a0d9a34ca103f384214f9c4ae876bc5bb0
Jardel Rodrigues <rodriguinhosilvards96@gmail.com>
2024-03-25 00:24:56 -0300

    Domain model User Role, seed

--- src/main/java/com/softstrem/dscommerce/entities/User.java
+++ src/main/java/com/softstrem/dscommerce/entities/User.java
@@ -3,13 +3,19 @@ package com.softstrem.dscommerce.entities;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -33,6 +39,26 @@ public class User implements Serializable {
 
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
+
+	@ManyToMany
+	@JoinTable(name = "tb_user_role",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+	
+	public User() {
+		
+	}
+	
+	public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.birthDate = birthDate;
+		this.password = password;
+	}
 
 	public Long getId() {
 		return id;
@@ -84,6 +110,40 @@ public class User implements Serializable {
 
 	public List<Order> getOrders() {
 		return orders;
+	}
+	
+	public Set<Role> getRoles(){
+		return roles;	
+	}
+
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+	
+	public boolean hasRole(String roleName) {
+		for(Role role : roles) {
+			if(role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
 	}
 
 	public static long getSerialversionuid() {
