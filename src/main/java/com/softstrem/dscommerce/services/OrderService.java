2c0bb7d48078788f0037602b0417df2aa46468b4
Jardel Rodrigues <rodriguinhosilvards96@gmail.com>
2024-04-04 23:07:40 -0300

    OrderDTO, get order by id

---
+++ src/main/java/com/softstrem/dscommerce/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.softstrem.dscommerce.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.softstrem.dscommerce.dto.OrderDTO;
+import com.softstrem.dscommerce.entities.Order;
+import com.softstrem.dscommerce.repositories.OrderRepository;
+import com.softstrem.dscommerce.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	@Transactional(readOnly = true)
+	public OrderDTO findById(Long id) {
+		Order order = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
+		return new OrderDTO(order);
+	}
+
+}
